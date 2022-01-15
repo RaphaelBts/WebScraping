4dc14ee80063e8dd54564eb24004acb375b7e555
Thomas Bouguet <t.bouguet@orange.fr>
2022-01-15 11:21:02 +0100

    API_v01

--- API-processing.py
+++
@@ -1,5 +0,0 @@
-
-print("mdr")
-# -*- coding: utf-8 -*-
-print("test01")
-print("test22")
---
+++ API/app.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import random as rd
+from math import *
+from datetime import datetime
+
+from flask import Flask,render_template,request
+
+app = Flask(__name__)
+
+@app.context_processor
+def inject_now():
+    return dict(now=datetime.now())
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
