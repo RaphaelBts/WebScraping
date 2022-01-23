20f1574f74943983dbe79d5056e8ce0ffebe82d3
Thomas Bouguet <t.bouguet@orange.fr>
2022-01-23 13:09:12 +0100

    Install pandas sur venv

--- API/app.py
+++ API/app.py
@@ -1,5 +1,5 @@
 #import numpy as np
-#import pandas as pd
+import pandas as pd
 #import matplotlib.pyplot as plt
 import random as rd
 from math import *
@@ -18,10 +18,23 @@ def index():
     return render_template('index.html')
 
 # For the future ameliorated versions : NOT DEVELOPPED YET
+# If we want to get a profitable business 
 @app.route("/subscription")
 def sub():
     return render_template('subsciption.html')
 
+
+@app.route("/search")
+def search():
+    if not 'new_prediction' in locals():
+        new_prediction=''
+    new_data=pd.DataFrame(data=request.form, index=[0])
+    ask=['Number of words in the title','Number of words in the text','Number of links','Number of images','Day of publication(monday...)','Subject(lifestyle,entertainement,buisness,socmed,tech,world)']
+    ask_type=['number','number','number','number','number','text','text']
+    print(new_data)
+    if not new_data.empty:
+        new_prediction=run_model(fill_row(new_data),seed=1977)
+    return render_template('search.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
